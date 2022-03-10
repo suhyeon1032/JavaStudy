78d718d8cc344c6d183254b43c9d49238405fce6
daram <79134336+suhyeon1032@users.noreply.github.com>
2022-03-10 20:39:36 +0900

    SpringMybatisMysql

--- Multicampas/Back-end/workspaceSpring/SpringMybatisMysql/src/main/java/com/campus/myapp/service/MemberService.java
+++ Multicampas/Back-end/workspaceSpring/SpringMybatisMysql/src/main/java/com/campus/myapp/service/MemberService.java
@@ -1,5 +1,12 @@
 package com.campus.myapp.service;
 
-public interface MemberService {
+import com.campus.myapp.vo.MemberVO;
 
+public interface MemberService {
+   //회원등록
+   public int memberInsert(MemberVO vo);
+   //로그인
+   public MemberVO loginCheck(MemberVO vo);
+   //회원정보수정폼
+   public MemberVO memberSelect(String userid);
 }
