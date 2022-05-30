dab30618ce07f4cb98d36a0d934cfa081c8fd042
suhyeon1032 <daram_9@naver.com>
2022-05-30 21:14:50 +0900

    Update BOJ_25186.java

--- Algorithm/BOJ_25186.java
+++ Algorithm/BOJ_25186.java
@@ -3,6 +3,7 @@ package week18;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ_25186 {
 
@@ -10,18 +11,25 @@ public class BOJ_25186 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int sum = 0;
-		long max = 0;
+		long max = 0; // 가장 많은 옷의 개수
 		int n = Integer.parseInt(br.readLine());
 		long arr[] = new long[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0;i<n;i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		for(int i=0;i<n;i++) {
 			max = (arr[i] > max) ? arr[i] : max;
 			sum += arr[i];
+			
+			if(sum<0) {
+				System.out.println("Happy");
+			}
 		}
 		if(n==1) {
 			System.out.println("Happy");
 		}
-		if(sum/2 >= max) {
+		else if(sum/2 >= max) {
 			System.out.println("Happy");
 		}else {
 			System.out.println("Unhappy");
