3cce6f85b7d67f1a7563af4d07d295c3a7730258
suhyeon1032 <daram_9@naver.com>
2022-06-18 02:34:40 +0900

    Update BOJ_1931.java

--- Algorithm/BOJ_1931.java
+++ Algorithm/BOJ_1931.java
@@ -1,59 +1,40 @@
-package week19;
-import java.util.Scanner;
+package week20;
+
 import java.util.Arrays;
 import java.util.Comparator;
- 
+import java.util.Scanner;
+
 public class BOJ_1931 {
- 
 	public static void main(String[] args) {
- 
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
 		
-		
-		/*
-		  time[][0] 은 시작시점을 의미 
-		  time[][1] 은 종료시점을 의미 
-		*/
 		int[][] time = new int[N][2];
 		
-		
-		for(int i = 0; i < N; i++) {
-			time[i][0] = in.nextInt();	// 시작시간 
-			time[i][1] = in.nextInt();	// 종료시간 
+		for(int i=0; i<N; i++) {
+			time[i][0] = sc.nextInt();
+			time[i][1] = sc.nextInt();
 		}
 		
-		
-		// 끝나는 시간을 기준으로 정렬하기 위해 compare 재정의 
 		Arrays.sort(time, new Comparator<int[]>() {
-			
+
 			@Override
 			public int compare(int[] o1, int[] o2) {
-				
-				// 종료시간이 같을 경우 시작시간이 빠른순으로 정렬해야한다.  
 				if(o1[1] == o2[1]) {
-					return o1[0] - o2[0];
+					return Integer.compare(o1[0], o2[0]);
 				}
-				
-				return o1[1] - o2[1];
+				return Integer.compare(o1[1], o2[1]);
 			}
- 
 		});
-		
-		int count = 0;
-		int prev_end_time = 0;
-		
-		for(int i = 0; i < N; i++) {
-			
-			// 직전 종료시간이 다음 회의 시작 시간보다 작거나 같다면 갱신 
-			if(prev_end_time <= time[i][0]) {
-				prev_end_time = time[i][1];
-				count++;
+		int cnt = 0;
+		int end = -1;
+		for(int i = 0; i<N; i++) {
+			if(time[i][0] >= end) {
+				end = time[i][1];
+				cnt++;
 			}
 		}
-		
-		System.out.println(count);
+		System.out.println(cnt);
 	}
- 
 }
