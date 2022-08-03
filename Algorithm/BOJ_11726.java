2590f5cba5e43037a0b299529624fec8c677cf9a
daram <79134336+suhyeon1032@users.noreply.github.com>
2022-08-03 20:22:01 +0900

    BOJ_11726_2×n 타일링

--- Algorithm/BOJ_11726.java
+++ Algorithm/BOJ_11726.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ_11726 {
+	// 2×n 타일링
 	static int n, dp[];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,13 +14,10 @@ public class BOJ_11726 {
 		dp = new int[n+2];
 		
 		dp[1] = 1;
-		dp[2] = 2;
-		dp[3] = 3;
-		dp[4] = 5;
-		dp[5] = 8; // 6->13 7->21 8->34 8->55
+		dp[2] = 2; // 4->5 5->8 6->13 7->21 8->34 8->55
 		
-		for(int i=6; i<=n; i++) {
-			dp[i] = dp[i-1] + dp[i-2];
+		for(int i=3; i<=n; i++) {
+			dp[i] = (dp[i-1] + dp[i-2])%10007;
 		}
 		System.out.println(dp[n]%10007);
 	}
